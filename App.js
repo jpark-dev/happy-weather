6d922f842fbc7c52445e9678674ac105879c85f2
Junsung Park <zeipar@gmail.com>
2020-07-21 21:10:18 -0700

    Installed expo-location to get geolocation. Implemented useState and UseEffect for initial loading with permission prompt for location access.

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import Loading from './Loading';
+import React, { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <Loading />
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      // Send to API and get weather
+      setIsLoading(false);
+    } catch (error) {
+      Alert.alert("Need location access!", "So sad");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return isLoading ? <Loading /> : null;
 }
