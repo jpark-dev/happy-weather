5cc2e0a86abe0f3065f9db3b9696752c869f745d
Junsung Park <zeipar@gmail.com>
2020-07-21 22:13:52 -0700

    Installed axios to fetch API. Created environment variable file.

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
 
+
+const API_KEY = "";
 export default function App() {
+
+  console.log(process.env);
   const [isLoading, setIsLoading] = useState(true);
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
 
   const getLocation = async () => {
     try {
@@ -12,6 +24,7 @@ export default function App() {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      getWeather(latitude, longitude);
       // Send to API and get weather
       setIsLoading(false);
     } catch (error) {
