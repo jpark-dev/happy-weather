2ef8afc1ba8c673d9bd5e81384d7422ffc65592d
Junsung Park <zeipar@gmail.com>
2020-07-21 20:48:10 -0700

    Created simple splash screen and refactored unnecessary components in App.js.

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Forbidden ya! </Text>
-      <Text style={styles.text}>Forbidden ya! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "black"
-  }
-});
